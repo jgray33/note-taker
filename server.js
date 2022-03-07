d935e0cdce5ca0075480e2a09988f5b590f0c3f3
Julia <julia.gray30@gmail.com>
2022-03-07 20:31:17 +0000

    Lined up public folder

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express")
 const app = express()
 
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/index.html")
     })
     
-
+app.get("/notes", (req,res) => {
+    res.sendFile(__dirname + "/public/notes.html")
+})
 
 app.listen(3000, function(){
     console.log("Listening")
